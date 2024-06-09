ea89c0425612e54fd4f94f496467665634dc7239
watchingdogs <69792238+watchingdogs@users.noreply.github.com>
2024-06-09 14:35:46 +0000

    before pages

--- utils/create_geojson.py
+++ utils/create_geojson.py
@@ -5,5 +5,29 @@ import requests
 import os
 
 
-PSApikey = os.environ["POSITIONSTACK_ACCESS_KEY"]
-requests.get(f"http://api.positionstack.com/v1/forward?access_key={PSApikey}&limit=1&country=HU&query={loc}").content
+
+GMAPS_KEY = os.environ["GMAPS_API_KEY"]
+
+kepzo_df = kepzoszervek_extractor.extractKepzoSzervek()
+institute_list = institute_extractor.get_institutes()
+institute_ids = sorted([i['nkhid'] for i in institute_list])
+edukretesz_geojson = edukretesz_to_geojson.extractEdukretesz()
+
+for id in institute_ids:
+    id_found = False
+    for feature in edukretesz_geojson['features']:
+        if feature['properties']['nkhid'] == id:
+            id_found = True
+            #feature['properties']['name'] = "New Name"
+            print(id)
+            print(feature['properties']['name'])
+            break
+            # két lehetőség van
+            # etitános, csak statisztikát kell berakni
+            
+    if not id_found:
+        print(f"ID {id} not found in the feature list.")
+        address = kepzo_df.loc[kepzo_df[2] == str(id)][3].values[0]
+        # nem etitános, teljesen új rekord kell, geocode stb
+        requests.get(f"https://maps.googleapis.com/maps/api/geocode/json?address={requests.utils.quote(address)}&key={GMAPS_KEY}").content
+
--- utils/edukretesz_to_geojson.py
+++ utils/edukretesz_to_geojson.py
@@ -5,7 +5,7 @@ import requests
 # Convert JSON data to GeoJSON format
 def extractEdukretesz():
     "Veszi az Edukresz API-ból (E-Titán) az iskolák adatait és Python GeoJSON formátumba alakítja."
-    data = requests.get('https://edukresz.hu/edukresz-partnerek/src/poi.php', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.3'})
+    data = requests.get('https://edukresz.hu/edukresz-partnerek/src/poi.php', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.3'}).json()
     geojson = {
         "type": "FeatureCollection",
         "features": []
