433b9911858c6b532341b9bf02af41ef6f0f6eac
watchingdogs <69792238+watchingdogs@users.noreply.github.com>
2024-07-27 15:29:27 +0000

    SEO

--- react/src/App.js
+++ react/src/App.js
@@ -204,8 +204,8 @@ function LocateUser() {
 
   const blueCircleSvg = `
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-    <circle cx="10" cy="10" r="10" fill="#fefdfe"/>
-    <circle cx="10" cy="10" r="8.5" fill="#005db7"/>
+    <circle cx="10" cy="10" r="9" fill="#fefdfe"/>
+    <circle cx="10" cy="10" r="7" fill="#0f53ff"/>
   </svg>
   `;
 
